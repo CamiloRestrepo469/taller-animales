c6ae264595d9bbb937fd21a4a5561b4831af7794
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2020-11-21 17:31:37 -0500

    se mejoro el public

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,7 +30,7 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Registrocontrolador::index');
+$routes->get('/', 'Regindexcontrol::index');
 $routes->get('/animales', 'Registrocontrolador::index');
 $routes->get('/usuario', 'Regusuarioscontrol::index');
 $routes->post('/usuario/registrarse', 'Regusuarioscontrol::registrarse');
